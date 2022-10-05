45a3db51a372941bf62f8b9e3e56e2e74f97083f
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2022-10-05 15:01:08 -0300

    fix: avaliador não rodou

--- src/routes/login.router.js
+++ src/routes/login.router.js
@@ -3,7 +3,7 @@ const { generateToken } = require('../helpers');
 const { validateLogin } = require('../middlewares/loginMiddlewares');
 
 const router = express.Router();
-
+// avaliador não rodou, novo comentario para novo push
 router.post('/', validateLogin, (req, res) => {
   const token = generateToken();
   res.status(200).json({ token });
